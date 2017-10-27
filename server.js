5e7a3c022b9d3d3693e58fe83e118a10d27aab4d
gaoziqi <571051761@qq.com>
2017-10-27 13:30:41 +0800

    2017-10-27 13:29

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var global = {
+    "data": {
+        "orientation": {"alpha": 0, "beta": 0, "gamma": 0},
+        "motion": {"acc": {"x": 0, "y": 0, "z": 0}}
+    }
+};
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
+app.get('/test', function (req, res) {
+    console.log(req.query.name);
+});
+app.get('/phone', function (req, res) {
+    res.sendFile(__dirname + "/public/phone.html");
+});
+app.post('/phone/orientation', function (req, res) {
+    global.data.orientation = req.body;
+    res.json(global.data.orientation);
+});
+app.post('/phone/motion', function (req, res) {
+    global.data.motion = req.body;
+    res.json(global.data.motion);
+});
+app.get('/main/data', function (req, res) {
+    res.json(global["data"]);
+});
+app.get('/main', function (req, res) {
+    res.sendFile(__dirname + "/public/main.html");
 });
 
 var server = app.listen(13017, function () {

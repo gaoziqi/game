d7dc21ac44192e4f37a25d099a222cd01da1fdda
gaoziqi <571051761@qq.com>
2017-10-27 17:04:39 +0800

    2017-10-27 17:04

--- public/js/game.js
+++ public/js/game.js
@@ -2,9 +2,11 @@ var scene = null;//主场景
 var camera = null;//主摄像头
 var renderer = null;//主渲染
 var controls = null;//主摄像头控制
-//var clock = new THREE.Clock();//主时钟
+var clock = new THREE.Clock();//主时钟
 var mouse = null;//主鼠标
 var phone = null;//主手机
+var position = new THREE.Vector3(0, 0, 0);
+var k_acc = 1;
 var euler = new THREE.Euler(); // 主手机旋转角
 var yUnit = new THREE.Vector3(0, 1, 0);
 var alpha = 0, beta = 90, gamma = 0;
@@ -18,8 +20,6 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    //var light = new THREE.PointLight(0xffffff);
-    //light.position.set(0, 250, 0);
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(-25, -25, 10);
     directionalLight.name = 'light0';
@@ -29,19 +29,22 @@ function init() {
     directionalLight1.name = 'light1';
     scene.add(directionalLight1);
 
-    var loader = new THREE.OBJLoader();
-    loader.load('obj/1.obj', function (object) {
-        object.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
-                //child.material.map = texture;
+    var mtlLoader = new THREE.MTLLoader();
+    mtlLoader.setPath('obj/');
+    mtlLoader.load('2.mtl', function (materials) {
+        materials.preload();
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.setPath('obj/');
+        objLoader.load('2.obj', function (object) {
+            phone = object;
+            var t = phone.children[0].geometry.center();
+            for (var i = 1; i < phone.children.length; i++) {
+                phone.children[i].geometry.translate(t.x, t.y, t.z);
             }
+            camera.lookAt(phone.position);
+            scene.add(phone);
         });
-        console.log(object);
-        object.children[0].geometry.computeBoundingBox();
-        object.children[0].geometry.center();
-        camera.lookAt(object.position);
-        phone = object;
-        scene.add(phone);
     });
 
     mouse = new THREE.Vector2();
@@ -63,8 +66,13 @@ function animate() {
         async: false,
         url: window.location.href + '/data',
         success: function (data) {
-            //var delta = clock.getDelta();
-
+            var delta = clock.getDelta();
+            /*delta *= delta * k_acc;
+            position.copy(phone.position);
+            position.x += data.motion.acc.x * 0.5 * delta;
+            position.y += data.motion.acc.y * 0.5 * delta;
+            position.z += data.motion.acc.z * 0.5 * delta;
+            phone.position.setScalar(0);*/
             var change = Math.abs(data.orientation.alpha - alpha) +
                 Math.abs(data.orientation.beta - beta) + Math.abs(data.orientation.gamma - gamma);
             if (change > 5.0 && change < 2000.0) {
@@ -80,10 +88,12 @@ function animate() {
                 beta = data.orientation.beta;
                 gamma = data.orientation.gamma;
             }
-            _update();
+            //phone.position.copy(position);
         }
     });
+    _update();
 }
+
 function _update() {
     controls.update();
 }
